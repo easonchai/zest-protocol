f159b3cf02bb74fa929a52f5292b7934bd56ae87
Eason Chai <echai2905@gmail.com>
2025-04-06 02:03:25 +0800

    feat: Add new header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { getEnsName } from "@/utils/api";
+import { ZestLogo } from "./zest-logo";
+import Link from "next/link";
 
 export function Header() {
   const { address } = useAccount();
@@ -24,8 +26,22 @@ export function Header() {
 
   return (
     <header className="flex items-center justify-between px-4 py-4">
-      <div className="flex items-center space-x-2">
-        <span className="text-xl font-bold">Zest Protocol</span>
+      <div className="flex items-center space-x-10">
+        <ZestLogo />
+        <div className="flex items-center space-x-7">
+          <Link href="/" className="text-primary font-medium cursor-pointer">
+            Staking
+          </Link>
+          <Link href="/pay" className="text-primary font-medium cursor-pointer">
+            Pay
+          </Link>
+          <Link
+            href="/borrow"
+            className="text-primary font-medium cursor-pointer"
+          >
+            Borrow
+          </Link>
+        </div>
       </div>
       <div className="flex items-center space-x-4">
         {ensName && <span className="text-sm text-gray-500">{ensName}</span>}
